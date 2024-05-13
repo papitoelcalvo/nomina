ba80db50f67493dbcbcfb33b4ce20bbc6eb716ea
LuisCal14 <luiscal1402@gmail.com>
2024-05-13 11:07:51 -0400

    ya se agregaron los demas listenis

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -13,6 +13,12 @@ const app = Express();
 app.use(Express.json());
 app.use(cors());
 
+
+/*################################################################################################################*/
+/*                                                     LISTAR                                                     */
+
+
+/*EMPLEADOS*/
 app.get("/empleados", (req, res) => {
   const sql =
     "SELECT empleado.idempleado, empleado.nombre_empleado, empleado.apellido, departamento.nombre_depar, puesto.nombre_pues, empresa.nombre, sueldos.sueldo FROM empleado INNER JOIN departamento ON departamento.iddepartamento = empleado.departamento INNER JOIN puesto ON puesto.idpuesto = empleado.puesto_emp  INNER JOIN empresa ON empresa.idempresa = empleado.empresa_emp INNER JOIN sueldos ON sueldos.idsueldos = empleado.sueldos_emp ";
@@ -22,10 +28,57 @@ app.get("/empleados", (req, res) => {
   });
 });
 
+
+/*ASISTENCIA*/ 
+app.get("/asistencia", (req, res) => {
+  const sql =
+    "SELECT asistencia.idasistencia, asistencia.entrada, asistencia.salida, empleado.nombre_empleado, departamento.nombre_depar, empresa.nombre FROM asistencia INNER JOIN departamento ON departamento.iddepartamento = asistencia.empleado_depa INNER JOIN empresa ON empresa.idempresa = asistencia.empleado_emp INNER JOIN empleado ON empleado.idempleado = asistencia.empleado_is";
+  db.query(sql, (err, result) => {
+    if (err) return res.json({ Mensaje: "error iniciando" });
+    return res.json(result);
+  });
+});
+
+
+/*CARGOS*/
+app.get("/cargos", (req, res) => {
+  const sql =
+    "SELECT * FROM puesto";
+  db.query(sql, (err, result) => {
+    if (err) return res.json({ Mensaje: "error iniciando" });
+    return res.json(result);
+  });
+});
+
+
+/*EMPRESA*/
+app.get("/empresa", (req, res) => {
+  const sql =
+    "SELECT * FROM empresa";
+  db.query(sql, (err, result) => {
+    if (err) return res.json({ Mensaje: "error iniciando" });
+    return res.json(result);
+  });
+});
+
+
+/*HORARIO*/
+app.get("/horario", (req, res) => {
+  const sql =
+    "SELECT * FROM horario";
+  db.query(sql, (err, result) => {
+    if (err) return res.json({ Mensaje: "error iniciando" });
+    return res.json(result);
+  });
+});
+/*################################################################################################################*/
+
+
+
 app.post("/usuarios/", (req, res) => {
-  const { id, nombre, apellido, departamento, puesto, empresa, sueldo } =
+  const { nombre, apellido, departamento, puesto, empresa, sueldo } =
     req.body;
-  const sql = `INSERT INTO empleado (idempleado, nombre_empleado, apellido, departamento, puesto_emp, empresa_emp, sueldos_emp) VALUES ('${id}', '${nombre}', '${apellido}', '${departamento}', '${puesto}', '${empresa}', '${sueldo}')`;
+  const sql = `INSERT INTO empleado (nombre_empleado, apellido, departamento, puesto_emp, empresa_emp, sueldos_emp, periodos_pago, horario_emp) VALUES ('${nombre}', '${apellido}', '${departamento}', '${puesto}', '${empresa}', '${sueldo}', '${periodo}', '${horario}')`;
   db.query(sql, (err, result) => {
     if (err) return res.json({ Mensaje: "error creando empleado" });
     return res.json({ Mensaje: "empleado creado" });
